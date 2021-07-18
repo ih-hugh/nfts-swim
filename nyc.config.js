9d91fc0dfa3dd9c68d39aa3078a7271f3bba3329
Hugo Cedano <hugoce17@gmail.com>
2021-07-17 22:32:27 -0500

    configured nyc

--- nyc.config.js
+++ nyc.config.js
@@ -1,22 +1,9 @@
 'use strict';
 
-const isWindows = require('is-windows')();
-
 module.exports = {
-  exclude: [
-    'coverage',
-    'self-coverage',
-    'test/fixtures/coverage.js',
-    'test/build/*',
-    'test/src/*',
-    'test/nyc.js',
-    'test/process-args.js',
-    'test/fixtures/_generateCoverage.js',
-  ],
-  /* Unknown why we don't get 100% coverage on Windows. */
-  'check-coverage': !isWindows,
-  branches: 100,
-  functions: 100,
-  lines: 100,
-  statements: 100,
+  extends: '@istanbuljs/nyc-config-typescript',
+  branches: 65,
+  functions: 65,
+  lines: 65,
+  statements: 75,
 };
